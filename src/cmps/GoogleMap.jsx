d3e51e7153d4cf5f8ab880da708b4ba604376b38
Andrey Chetvergov <andrey32102@gmail.com>
2024-12-05 20:10:52 +0200

    update GoogleMap.jsx

--- src/cmps/GoogleMap.jsx
+++ src/cmps/GoogleMap.jsx
@@ -24,7 +24,7 @@ export function GoogleMap() {
             >
                 <AnyReactComponent
                     {...coordinates}
-                    text="🌓"
+                    text="🙂"
                 />
             </GoogleMapReact>
         </div>
