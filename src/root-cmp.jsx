93b48dabe2d799f9d76fb12131759e9b7e452eb7
Andrey Chetvergov <andrey32102@gmail.com>
2024-12-27 12:46:07 +0200

    Add store

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -11,31 +11,34 @@ import { MyChart } from './cmps/MyChart.jsx'
 import { MaterialUi } from './cmps/MaterialUi.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { UserProfile } from './pages/UserProfile.jsx'
+import { store } from './store/store'
 import { AdminDashboard } from './pages/AdminDashboard.jsx'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 export function App() {
     return (
-        <Router>
-            <div className='main-app'>
-                <AppHeader />
-                <main className='container'>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/toy" element={<ToyIndex />} />
-                        <Route path="/toy/:toyId" element={<ToyDetails />} />
-                        <Route path="/map" element={<GoogleMap />} />
-                        <Route path="/form" element={<MyForm />} />
-                        <Route path="/chart" element={<MyChart />} />
-                        <Route path="/mui" element={<MaterialUi />} />
-                        <Route path="/review" element={<ReviewIndex />} />
-                        <Route path="/about" element={<AboutUs />} />
-                        <Route path="/user" element={<UserProfile />} />
-                        <Route path="/admin" element={<AdminDashboard />} />
-                    </Routes>
-                </main>
-                <AppFooter />
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className='main-app'>
+                    <AppHeader />
+                    <main className='container'>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/toy" element={<ToyIndex />} />
+                            <Route path="/toy/:toyId" element={<ToyDetails />} />
+                            <Route path="/map" element={<GoogleMap />} />
+                            <Route path="/form" element={<MyForm />} />
+                            <Route path="/chart" element={<MyChart />} />
+                            <Route path="/mui" element={<MaterialUi />} />
+                            <Route path="/review" element={<ReviewIndex />} />
+                            <Route path="/about" element={<AboutUs />} />
+                            <Route path="/user" element={<UserProfile />} />
+                            <Route path="/admin" element={<AdminDashboard />} />
+                        </Routes>
+                    </main>
+                    <AppFooter />
+                </div>
+            </Router>
+        </Provider>
     )
 }
